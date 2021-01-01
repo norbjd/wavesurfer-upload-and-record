7c34d7e052eaffc546bd64eb5559fc8947e17afc
norbjd <norbjd@users.noreply.github.com>
2021-01-01 15:31:56 +0100

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## What is it ?
+
+Small example demonstrating how to :
+- upload audio
+- display it with [wavesurfer.js](https://github.com/katspaugh/wavesurfer.js) for playing
+- recording part of the audio and exporting it
+
+## Issue
+
+After clicking the "record" button, **sound is not played anymore on the webpage**. The exported audio can be correctly read with VLC.
+
+## Notes
+
+`demo.wav` file comes from `https://github.com/katspaugh/wavesurfer.js/blob/3.3.0/example/media/demo.wav`.
---
+++ main.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.getElementById("upload").addEventListener("change", function () {
+        fr = new FileReader();
+        fr.readAsDataURL(document.getElementById("upload").files[0]);
+        fr.onload = (e) => {
+            var audio = document.createElement('audio');
+            audio.src = e.target.result;
+            initWavesurfer(audio);
+        };
+        // remove upload button
+        document.getElementById("upload").outerHTML = "";
+    });
+
+    function initWavesurfer(audio) {
+        const wavesurfer = WaveSurfer.create({
+            container: document.querySelector('#waveform'),
+            backend: 'MediaElement',
+            mediaControls: false
+        });
+
+        wavesurfer.on('error', function (e) {
+            console.warn(e);
+        });
+
+        wavesurfer.load(audio);
+
+        document.getElementById("controls").style.display = 'block';
+
+        document
+            .querySelector('[data-action="play"]')
+            .addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
+
+        document
+            .querySelector('[data-action="record"]')
+            .addEventListener('click', () => {
+                const audioCtx = new AudioContext();
+                const dest = audioCtx.createMediaStreamDestination();
+                const audioStream = dest.stream;
+
+                audioCtx.createMediaElementSource(audio).connect(dest);
+
+                const chunks = [];
+                const rec = new MediaRecorder(new MediaStream([audioStream.getAudioTracks()[0]]));
+                rec.ondataavailable = (e) => {
+                    chunks.push(e.data);
+                }
+                rec.onstop = () => {
+                    const blob = new Blob(chunks, { type: "audio/ogg" });
+                    const a = document.createElement("a");
+                    a.download = "export.ogg";
+                    a.href = URL.createObjectURL(blob);
+                    a.textContent = "export the audio";
+                    a.click();
+                    window.URL.revokeObjectURL(a.href);
+                }
+
+                wavesurfer.play();
+                rec.start();
+
+                setTimeout(() => {
+                    rec.stop();
+                    wavesurfer.stop();
+                    audioCtx.close();
+                }, 5 * 1000);
+            });
+    }
+});
